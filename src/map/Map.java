1e6ce3749a1ffe86fd910950f13b58df6d8ff671
aghatge738 <aghatge738@PR22.hhs.win.fuhsd.org>
2017-05-16 09:01:24 -0700

    Updated some classes in the map package

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,5 +1,36 @@
 package map;
 
+import java.awt.Graphics;
+
+import units.Unit;
+
 public class Map {
+	
+	private Tile[][] map;
+	
+	public Map() {
+		map = new Tile[15][15];
+	}
+	
+	public Tile getTile(int r, int c) {
+		if(r >= 15 || r < 0) {
+			if(c >= 15 || c < 0) {
+				return map[r][c];
+			}
+		}
+		return null;
+	}
+	
+	public Unit getAdjacentObject() {
+		
+		
+		
+		return null;
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
 
 }

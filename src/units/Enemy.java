967e4ff597f9e20d17ae047836f2744a22652fec
ewan633 <ewan633@PR30.hhs.win.fuhsd.org>
2017-05-16 08:06:22 -0700

    Added "Enemy" class

---
+++ src/units/Enemy.java
@@ -0,0 +1,22 @@
+package units;
+
+public class Enemy extends Unit 
+{
+	private int hp;
+	private int power;
+	private int level;
+	
+	public Enemy()
+	{
+		super();
+		
+	}
+	
+	public Enemy(int hp, int power, int level)
+	{
+		super();
+		
+	}
+	
+	
+}

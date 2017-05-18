e2a00f1c510fe96bb7b54d478e133f087495a29f
aghatge738 <aghatge738@PR22.hhs.win.fuhsd.org>
2017-05-18 08:48:19 -0700

    Finished

--- src/map/MovementPhase.java
+++ src/map/MovementPhase.java
@@ -10,8 +10,18 @@ public class MovementPhase {
 		unit = u;
 	}
 	
-	public void run(Unit u, int xCor, int yCor, Map map) {
+	public void run(int r, int c, Map map) {
+		int range = unit.getMovementDistance();
+		int currentRow = map.getUnitRow(unit);
+		int currentCol = map.getUnitCol(unit);
 		
+		if(r < 15 && r >= 0 && c < 15 &&  c >= 0) {
+			if(r < currentRow+range && r > currentRow+range && c < currentCol+range && c > currentCol+range) {
+				if(map.getObjectAtSpot(currentRow, currentCol).equals(null)) {
+					map.changeUnitPlace(unit, r, c);
+				}
+			}
+		}
 		
 	}
 
